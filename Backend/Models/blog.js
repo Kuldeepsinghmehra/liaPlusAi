4d6343cb7878d2a228fcf69c3aff1367ef035c21
kuldeepsmehra <kuldeepm272@gmail.com>
2025-04-25 17:13:49 +0530

    Added models in daabase

---
+++ Backend/Models/blog.js
@@ -0,0 +1,8 @@
+const mongoose =require('mongoose')
+const blogSchema=new mongoose.Schema({
+    title: String,
+    content:string,
+    author:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
+    createdAt:{type:Date,default:Date.now}
+})
+module.exports=mongoose.model('Blog',blogSchema)
